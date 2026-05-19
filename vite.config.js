31ba2ab9d026b586547aa594159ae4f4a9c77ff4
filipebritocosta88 <filipebritocosta88@gmail.com>
2026-05-19 14:53:36 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', 
+  // O GitHub Pages coloca seu site em uma subpasta com o nome do repositório
+  base: '/logistica/', 
 })
